4d8971dd0fefd7393063d6e11035204bc22e5404
Ade Hermawan <app@adehermawan.com>
2021-09-23 08:40:20 +0700

    update login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,12 +6,12 @@ function Login(props) {
   const username = useFormInput('');
   const password = useFormInput('');
 //  const role = useFormInput('');
-  const [role, setRole] = useState('');
+  const [role, setRole] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   var apiUrl = '';
   //const [apiUrl, setApiUrl] = useState('');
-//console.log(role);
+console.log(role);
 
 
   // handle button click of login form
@@ -51,9 +51,9 @@ function Login(props) {
       </div>
       <div>
         <p>Please select your roles:</p>
-          <input type="radio" id="transporter" name="role" value="transporter" />
+          <input type="radio" id="transporter" name="role" value="transporter" onChange={setRole('transporter')} />
           <label for="transporter">transporter</label><br/>
-          <input type="radio" id="shipper" name="role" value="shipper" />
+          <input type="radio" id="shipper" name="role" value="shipper"  onChange={setRole('shipper')} />
           <label for="shipper">shipper</label>
       </div>
       {error && <><small style={{ color: 'red' }}>{error}</small><br /></>}<br />
